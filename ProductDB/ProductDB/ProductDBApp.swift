d7af76489640fa5741655205094bb6193edb9a2a
Farbod <maani@Farbods-Mac-Mini.local>
2025-04-09 14:48:43 +0330

    Created the project + added the AppDomain package

---
+++ ProductDB/ProductDB/ProductDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDBApp.swift
+//  ProductDB
+//
+//  Created by Farbod on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
