e0abd1dcfd6baf0d5ddadef780ddc59354ab8a49
Farbod <maani@Farbods-Mac-Mini.local>
2025-04-09 16:00:35 +0330

    Added item view

--- AppUI/Sources/AppUI/Item/ItemView.swift
+++ AppUI/Sources/AppUI/Item/ItemView.swift
@@ -8,4 +8,36 @@
 import SwiftUI
 
 public struct ItemView: View {
+    private var model: ItemViewModel
+    
+    public init(model: ItemViewModel) {
+        self.model = model
+    }
+    
+    public var body: some View {
+        VStack {
+            Text(model.title)
+                .font(.headline)
+            
+            HStack{
+                Image(systemName: "")
+            }
+            .frame(width: 100, height: 150)
+            
+            Spacer()
+            
+            Text(model.description)
+                .font(.caption)
+            
+            Spacer()
+            
+            VStack {
+                Text("\(model.rate)")
+                    .font(.caption)
+                
+                Text("\(model.price) $")
+                    .font(.caption)
+            }
+        }
+    }
 }
